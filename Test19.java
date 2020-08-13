c1c19fbe3429cffe64af4548c74e57c0aa33b0dd
yalim4049 <yalim4049@naver.com>
2020-08-13 14:27:28 +0900

    step1

---
+++ Test19.java
@@ -0,0 +1,12 @@
+public class Test19{
+	public static void main(String[] args){
+	int score=90;
+	String result="";
+
+	if(score>=60)
+		result="pass";
+	else 
+		result="fail";
+	System.out.println(result);
+	}
+}
